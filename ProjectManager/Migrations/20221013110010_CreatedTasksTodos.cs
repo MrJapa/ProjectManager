4d62806fb020b00f93cca233ad4fd30a17a73e26
J. Japa <jjv@redmark.dk>
2022-10-13 13:45:36 +0200

    Opgave 2.1 - Tasks og todos

---
+++ ProjectManager/Migrations/20221013110010_CreatedTasksTodos.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManager.Migrations
+{
+    public partial class CreatedTasksTodos : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.TaskId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDo",
+                columns: table => new
+                {
+                    ToDoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    IsComplete = table.Column<bool>(type: "INTEGER", nullable: false),
+                    TaskId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDo", x => x.ToDoId);
+                    table.ForeignKey(
+                        name: "FK_ToDo_Tasks_TaskId",
+                        column: x => x.TaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "TaskId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDo_TaskId",
+                table: "ToDo",
+                column: "TaskId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDo");
+
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ ProjectManager/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static void Main(string[] Args)
+    {
+        using (ProjectContext context = new ProjectContext())
+        {
+            var tasks = context.Tasks.Include(task => task.ToDo);
+            foreach (var task in tasks)
+            {
+                Console.WriteLine($"Task: {task.Name}");
+                foreach(var todo in task.ToDo)
+                {
+                    Console.WriteLine($"- {todo.Name}");
+                }
+            }
+        }
+    }
+    public static void seedTasks()
+    {
+        using var db = new ProjectContext();
+        Task t1 = new Task();
+        t1.Name = "Produce software";
+        t1.ToDo.Add(new ToDo { Name = "Write code", IsComplete = false });
+        t1.ToDo.Add(new ToDo { Name = "Compile source", IsComplete = false });
+        t1.ToDo.Add(new ToDo { Name = "Test program", IsComplete = false });
+
+        db.Add(t1);
+
+        Task t2 = new Task();
+        t2.Name = "Brew coffee";
+        t2.ToDo.Add(new ToDo { Name = "Pour water",IsComplete = false });
+        t2.ToDo.Add(new ToDo { Name = "Pour coffee", IsComplete = false });
+        t2.ToDo.Add(new ToDo { Name = "Turn on", IsComplete = false });
+
+        db.Add(t2);
+
+        db.SaveChanges();
+    }
+}
+public class ProjectContext : DbContext
+{
+    public DbSet<Task> Tasks { get; set; }
+    public DbSet<ToDo> ToDo { get; set; }
+
+    public string DbPath { get; }
+
+    public ProjectContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "ProjectManager.db");
+    }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Task
+{
+    public int TaskId { get; set; }
+    public string Name { get; set; }
+
+    public List<ToDo> ToDo { get; } = new();
+}
+
+public class ToDo
+{
+    public int ToDoId { get; set; }
+    public string Name { get; set; }
+    public bool IsComplete { get; set; }
+}
